02c90fa486f179b0a67573b82417a547639db7be
Charles Meyers <cmmeyers23@gmail.com>
2022-10-12 20:19:46 -0700

    Return number of litres drank

---
+++ js/cyclingLitres.js
@@ -0,0 +1,7 @@
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// Solution
+
+const litres = time => Math.floor(time/2);
