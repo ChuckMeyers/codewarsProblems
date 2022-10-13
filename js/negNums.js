0025fc95db467c04b535215f043064647fc42f00
Charles Meyers <cmmeyers23@gmail.com>
2022-10-13 08:23:15 -0700

    Make a number negative

---
+++ js/negNums.js
@@ -0,0 +1,5 @@
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Solution
+
+const makeNegative = num => num > 0 ? -num : num;
