a3ed4eca7ff2cd9214c478101170c9aada646918
Charles Meyers <cmmeyers23@gmail.com>
2022-10-25 16:29:47 -0700

    Return first n elements of arr

---
+++ js/takeFirstElements.js
@@ -0,0 +1,6 @@
+// Create a function that accepts a list/array and a number n, 
+//and returns a list/array of the first n elements from the list/array.
+
+// Solution
+
+const take = (arr, n) => arr.slice(0, n);
