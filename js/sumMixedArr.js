77ef39f4f6044c163a99e4a9804af1f3d93a33a5
Charles Meyers <cmmeyers23@gmail.com>
2022-10-27 20:51:46 -0700

    Check for strings and add nums together

---
+++ js/sumMixedArr.js
@@ -0,0 +1,7 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+// Solution
+
+const sumMix = x => x.reduce((a,b) => +b+a, 0)
+
