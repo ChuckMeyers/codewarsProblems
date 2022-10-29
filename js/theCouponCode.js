9d0914556ecdaf238c1862e84852fd89718ebe3a
Charles Meyers <cmmeyers23@gmail.com>
2022-10-28 18:19:24 -0700

    Check for fake or expired coupons

---
+++ js/theCouponCode.js
@@ -0,0 +1,12 @@
+// Your online store likes to give out coupons for special occasions. 
+// Some customers try to cheat the system by entering invalid codes or using expired coupons.
+// Task     
+// Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+// Solution
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+  }
