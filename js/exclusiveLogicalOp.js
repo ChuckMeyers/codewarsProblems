4b569203f152c202e077c58af8cfd43163ef4efa
Charles Meyers <cmmeyers23@gmail.com>
2022-10-30 11:55:19 -0700

    Compare booleans

---
+++ js/exclusiveLogicalOp.js
@@ -0,0 +1,7 @@
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), 
+// your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. 
+// Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+
+// Solution
+
+const xor = (a,b) => a!=b;
