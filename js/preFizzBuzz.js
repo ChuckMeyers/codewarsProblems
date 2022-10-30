73ed72cd8f497e6f61fd5abd9b18bc749b6dbdad
Charles Meyers <cmmeyers23@gmail.com>
2022-10-30 10:55:28 -0700

    Create list from 1 to n

---
+++ js/preFizzBuzz.js
@@ -0,0 +1,14 @@
+// This is the first step to understanding FizzBuzz.
+// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+// Your expected output is an array of positive integers from 1 to n (inclusive).
+// Your job is to write an algorithm that gets you from the input to the output.
+
+// Solution
+
+function preFizz(n) {
+    let numsHere = [];
+    for(let i = 1; i <= n; i++){
+      numsHere.push(i);
+    }
+    return numsHere;
+  };
