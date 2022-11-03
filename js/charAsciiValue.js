763c794a91e933b2214e3572e27e4d16e5d1508a
Charles Meyers <cmmeyers23@gmail.com>
2022-11-03 16:18:37 -0700

    Return ASCII char for numer input

---
+++ js/charAsciiValue.js
@@ -0,0 +1,5 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Solution
+
+const getChar = c => String.fromCharCode(c);
