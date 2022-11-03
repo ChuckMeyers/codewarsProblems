65e881a75387fef228829314e851864c180328d0
Charles Meyers <cmmeyers23@gmail.com>
2022-11-03 16:19:23 -0700

    String practice

---
+++ js/basicDataStrings.js
@@ -0,0 +1,19 @@
+// In javascript, String is one of basic data types. Use "" or '' to declare a string variable. 
+// It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+// String can use operators +, Connect two or more strings together.
+// Task
+// misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, 
+// and return them(Please note the uppercase and lowercase letters are different).
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// Solution
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+
+const Dad = () => d1 + a2 + d2
+const Bee = () => b1 + e2 + e2
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2
+
+const answer1 = () => "no"
+const answer2 = () => "no"
+const answer3 = () => "yes"
