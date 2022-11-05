52c0478551ebf3c626abc2969ec9e92a8f5cb50a
Charles Meyers <cmmeyers23@gmail.com>
2022-11-05 12:28:40 -0700

    Does list include item

---
+++ js/doesListInclude.js
@@ -0,0 +1,5 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+// Solution
+
+const include = (arr, item) => arr.includes(item);
