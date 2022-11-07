1b76fec3f2f469954b3f750cfa1ae136d2c39bde
Charles Meyers <cmmeyers23@gmail.com>
2022-11-06 20:58:45 -0800

    Return string based on conditional

---
+++ js/appleTurnover.js
@@ -0,0 +1,7 @@
+// Your job is simple, 
+// if x squared is more than 1000, return It's hotter than the sun!!, 
+// else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Solution
+
+const apple = x => x**2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
