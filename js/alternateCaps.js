a7806abf22baa30b8442be65ae4f2318401a9361
Charles Meyers <cmmeyers23@gmail.com>
2022-11-10 18:03:32 -0800

    Alternate capitals by odd or even index

---
+++ js/alternateCaps.js
@@ -0,0 +1,10 @@
+//Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// Solution
+
+function capitalize(s){
+    const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+    const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+    return [even, odd];
+  };
+  
