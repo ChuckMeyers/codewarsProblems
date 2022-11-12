a5a37c988978298f3953d5ab3fbfabbf40fcc94d
Charles Meyers <cmmeyers23@gmail.com>
2022-11-12 09:27:58 -0800

    Return string with commas between elements

---
+++ js/commaDelimiters.js
@@ -0,0 +1,7 @@
+// Input: Array of elements
+// ["h","o","l","a"]
+// Output: String with comma delimited elements of the array in th same order.
+
+// Solution
+
+const printArray = array => array.join(',')
