433a7ea74276ac90bc135dcff38c087a9b2ee8ad
Charles Meyers <cmmeyers23@gmail.com>
2022-11-13 21:10:13 -0800

    Square each digit of num

---
+++ js/squareEveryDigit.js
@@ -0,0 +1,7 @@
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
+
+// Solution
+
+const squareDigits = num => Number(num.toString().split('').map(x => Math.pow(x,2)).join(''))   
