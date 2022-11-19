8f1c1afd82b2ebb837489772eb1127dd9b39056f
Charles Meyers <cmmeyers23@gmail.com>
2022-11-18 21:50:33 -0800

    Check if array contains x

---
+++ js/onlyNeedOne.js
@@ -0,0 +1,5 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Solution
+
+const check = (a,x) => a.includes(x);
