512fb6a1009f546f19e643a2a289b2e9e56b75c3
Charles Meyers <cmmeyers23@gmail.com>
2022-11-26 12:59:24 -0800

    Return nums that are multiples of their index

---
+++ js/multipleOfIndex.js
@@ -0,0 +1,5 @@
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+// Solution 
+
+const multipleOfIndex = array => array.filter((num, ind) => num % ind === 1);
