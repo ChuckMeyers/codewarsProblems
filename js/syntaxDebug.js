307972d9a7e9a3e09393e41a1b961f8a032b97ea
Charles Meyers <cmmeyers23@gmail.com>
2022-11-30 21:39:34 -0800

    Debug student's code

---
+++ js/syntaxDebug.js
@@ -0,0 +1,11 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+// Solution
+
+function main (verb, noun){
+    return verb + noun
+  }
