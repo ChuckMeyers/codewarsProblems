561d5fa9cd0d0e7d7e0b806ae4ed1f766dd61bf4
Charles Meyers <104474011+ChuckMeyers@users.noreply.github.com>
2023-01-04 11:34:33 -0800

    Write a for loop to determine even or odd num

---
+++ js/loopStatementFor.js
@@ -0,0 +1,18 @@
+// The function pickIt accepts 1 parameter, arr, which is an array of numbers. 
+// We need to traverse arr by using a for loop. If an element is an odd number, push it to the odd array, and if it's an even number, push it to the evenarray.
+
+// Solution
+
+function pickIt(arr){
+    var odd=[],even=[];
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] % 2 === 0){
+        even.push(arr[i])
+      } else {
+        odd.push(arr[i])
+      }
+    }
+    
+    
+    return [odd,even];
+  }
